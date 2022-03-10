4ee498e8c820d47729e12d48bfa81907e2190a87
Juzer1 <95688069+Juzer1@users.noreply.github.com>
2022-03-10 04:30:12 -0800

    Add files via upload
    noob

---
+++ kwitter_room.js
@@ -0,0 +1,39 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyDWBxex5zaMQD_tcmOHRsmwVP51jmL8OxU",
+      authDomain: "letschat-dfec3.firebaseapp.com",
+      databaseURL: "https://letschat-dfec3-default-rtdb.firebaseio.com",
+      projectId: "letschat-dfec3",
+      storageBucket: "letschat-dfec3.appspot.com",
+      messagingSenderId: "511990592913",
+      appId: "1:511990592913:web:3a04603b76eb17fce95630"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    function addRoom()
+    {
+          room_name = document.getElementById("room_name").value;
+
+
+          firebase.database().ref("/").child(room_name).update({
+                purpose : "adding room name"
+          });
+          localStorage.setItem("room_name",room_name);
+
+          window.location = "kwitter_page.html"
+    }
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+function redirectToRoomName(name)
+{
+      localStorage.setItem("room_name",name);
+      window.location = "kwitter_page.html";
+}
